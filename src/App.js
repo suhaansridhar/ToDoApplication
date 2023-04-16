34ea8ae71ef6588974d341a2691ba70c2bda61cf
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-16 16:04:04 +0100

    deleted Components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import React from "react";
-import TaskGenerator from "./Components/TaskGenerator";
-import {nanoid} from 'nanoid';
-import AddTask from "./Components/AddTask";
-import TaskList from "./Components/TaskList";
+import React from 'react'
 
-function App() {
-  const [tasks, setTasks] = React.useState([
-    {id: nanoid(), taskName: 'Brushing'}, 
-  ]);
-
-  function removeTask(id){
-    console.log(id)
-    let tempArray = tasks.map(item => item.id !== id);
-    setTasks(tempArray);
-}
-
-function addTask(content){
-  let tempId = nanoid();
-  const tempObj = {
-    id: tempId,
-    taskName: content
-  }
-  setTasks(prevArray => [...prevArray, tempObj]);
-}
-
+const App = () => {
   return (
-    <div className="Main--Container">
-      <AddTask addTask = {addTask}/>
-      <TaskList taskList={tasks} removeTask={removeTask}/>
+    <div>
+      hellp
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
