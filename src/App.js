9a8efc41cb3d24efb02ba1ca025642318d820e74
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-16 10:55:08 +0100

    created addtask component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import TaskGenerator from "./Components/TaskGenerator";
 import {nanoid} from 'nanoid';
+import AddTask from "./Components/AddTask";
 
 function App() {
   const [tasks, setTasks] = React.useState([{id: nanoid(), taskName: 'Brushing'}]);
 
-  let arrayOfTasks = tasks.map(item => <TaskGenerator taskName = {item.taskName} key={item.id}/>)
+  function removeTask(id){
+    const tempArray = tasks.map(item => item.id !== id);
+    setTasks(tempArray);
+}
+
+  let arrayOfTasks = tasks.map(item => <TaskGenerator taskName = {item.taskName} key={item.id} id={item.id} removeTask={removeTask}/>)
 
   return (
     <div className="Main--Container">
+      <AddTask />
       {arrayOfTasks}
     </div>
   );
--- src/Components/TaskGenerator.js
+++ src/Components/TaskGenerator.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import {AiFillDelete} from 'react-icons/ai';
+import {GrCheckbox} from 'react-icons/gr';
 
-const TaskGenerator = () => {
+const TaskGenerator = ({taskName, id, removeTask}) => {
   return (
     <div className='task--generator--container'>
-
+        <div className="task--generator--middle--container">
+            <div>
+                <GrCheckbox />
+                <h3>{taskName}</h3>
+            </div>
+            <button onClick={() => removeTask(id)}><AiFillDelete /></button>
+        </div>
     </div>
   )
 }
