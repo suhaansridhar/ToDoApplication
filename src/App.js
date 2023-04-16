ef68395e4512ee5b9947798c93d01484c39fd5cc
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-16 10:47:18 +0100

    adding taskgenerator component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TaskGenerator from "./Components/TaskGenerator";
+import {nanoid} from 'nanoid';
 
 function App() {
+  const [tasks, setTasks] = React.useState([{id: nanoid(), taskName: 'Brushing'}]);
+
+  let arrayOfTasks = tasks.map(item => <TaskGenerator taskName = {item.taskName} key={item.id}/>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main--Container">
+      {arrayOfTasks}
     </div>
   );
 }
---
+++ src/Components/TaskGenerator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskGenerator = () => {
+  return (
+    <div className='task--generator--container'>
+
+    </div>
+  )
+}
+
+export default TaskGenerator
